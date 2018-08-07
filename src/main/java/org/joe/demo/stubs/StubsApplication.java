1b3f2e77964a347ddc10d96803249bc3e2d9b203
joe <joseph.gonzalez@upr.edu>
2018-08-07 00:33:43 -0400

    Initial commit includes project dir and dependencies

---
+++ src/main/java/org/joe/demo/stubs/StubsApplication.java
@@ -0,0 +1,12 @@
+package org.joe.demo.stubs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StubsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StubsApplication.class, args);
+	}
+}
---
+++ src/test/java/org/joe/demo/stubs/StubsApplicationTests.java
@@ -0,0 +1,16 @@
+package org.joe.demo.stubs;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class StubsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
