d2819b4cff7201849ba5b464137b3db24b530e94
joe <joseph.gonzalez@upr.edu>
2018-08-14 08:13:55 -0400

    adding proxy project

--- proxy/src/main/java/org/joe/demo/proxy/ProxyApplication.java
+++ proxy/src/main/java/org/joe/demo/proxy/ProxyApplication.java
@@ -1,14 +1,16 @@
-package org.joe.demo.stubs;
+package org.joe.demo.proxy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
-@SpringBootApplication
+@EnableZuulProxy
 @EnableOAuth2Sso
-public class StubsApplication {
+@SpringBootApplication
+public class ProxyApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(StubsApplication.class, args);
+		SpringApplication.run(ProxyApplication.class, args);
 	}
 }
