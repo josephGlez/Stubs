69e6720c8c18bf4247d9c8b7c089e9c512abaa3e
joe <joseph.gonzalez@upr.edu>
2018-08-17 00:39:30 -0400

    added swagger added angular support in the backend added RepositoryRestResource

--- Stubs/src/main/java/org/joe/demo/stubs/controller/RequestMappingShortcutsController.java
+++ Stubs/src/main/java/org/joe/demo/stubs/controller/RequestMappingShortcutsController.java
@@ -5,6 +5,7 @@ import java.util.Collection;
 import org.joe.demo.stubs.persistence.model.PersonEntity;
 import org.joe.demo.stubs.services.PersonService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,6 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/person")
+@CrossOrigin(origins = "http://localhost:4200") //angular app
 public class RequestMappingShortcutsController {
 
 	@Autowired
--- Stubs/src/main/java/org/joe/demo/stubs/persistence/dao/PersonDAO.java
+++ Stubs/src/main/java/org/joe/demo/stubs/persistence/dao/PersonDAO.java
@@ -6,9 +6,19 @@ import org.joe.demo.stubs.persistence.model.PersonEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+//@RepositoryRestResource exposes all JpaRepository DAO methods as RESTful endpoints
+// http://localhost:8081/people
+@RepositoryRestResource(path = "people")
+@CrossOrigin(origins = "http://localhost:4200")//angular app
 public interface PersonDAO extends JpaRepository<PersonEntity, Long> {
 	
+	//Exposes custom queries as RESTful endpoints under the {DAO_Path}/search/{path}?{param}={val}
+	// http://localhost:8081/people/search/age?age=18
+	@RestResource
 	@Query(value = "SELECT * FROM public.person WHERE person.age <= :age", nativeQuery = true)
 	Collection<PersonEntity> getPeopleByAge(@Param("age") Integer age);
 
